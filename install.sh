9692ef884a3f35387bd0df1694115e976ce89ac7
Cameron Taylor <axis7818@gmail.com>
2020-04-10 15:21:06 -0700

    dont force link

--- install.sh
+++ install.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
 
+echo "Installing dotfiles. If a file exists, it will not be overridden"
+
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # git
 echo "linking gitconfig..."
-ln -sf ${BASEDIR}/git/gitconfig ~/.gitconfig
+ln -s ${BASEDIR}/git/gitconfig ~/.gitconfig
 
 # tmux
 echo "linking tmux config..."
-ln -sf ${BASEDIR}/tmux/tmux.conf ~/.tmux.conf
+ln -s ${BASEDIR}/tmux/tmux.conf ~/.tmux.conf
 
 # nvim (make sure neovim is installed)
 echo "linking neovim config..."
 mkdir -p ~/.config/nvim
-ln -sf ${BASEDIR}/nvim/init.vim ~/.config/nvim/init.vim
-ln -sf ${BASEDIR}/nvim/colors/onedark.vim ~/.config/nvim/colors/onedark.vim
-ln -sf ${BASEDIR}/nvim/autoload/onedark.vim ~/.config/nvim/autoload/onedark.vim
+ln -s ${BASEDIR}/nvim/init.vim ~/.config/nvim/init.vim
+ln -s ${BASEDIR}/nvim/colors/onedark.vim ~/.config/nvim/colors/onedark.vim
+ln -s ${BASEDIR}/nvim/autoload/onedark.vim ~/.config/nvim/autoload/onedark.vim
 
 # zsh
 echo "linking zsh config..."
-ln -sf ${BASEDIR}/zsh/zshrc ~/.zshrc
-ln -sf ${BASEDIR}/zsh/custom/themes/axis7818.zsh-theme ~/.oh-my-zsh/custom/themes/axis7818.zsh-theme
+ln -s ${BASEDIR}/zsh/zshrc ~/.zshrc
+ln -s ${BASEDIR}/zsh/custom/themes/axis7818.zsh-theme ~/.oh-my-zsh/custom/themes/axis7818.zsh-theme
